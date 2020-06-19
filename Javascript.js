9ac3930baf268ac364d602d54fb21f59ae2b3902
ashwin839 <66672443+ashwin839@users.noreply.github.com>
2020-06-19 15:55:54 +0530

    Add files via upload

---
+++ Javascript.js
@@ -0,0 +1,204 @@
+/**************************
+ * Variable and data types
+ */
+/*
+var firstName =  'Ashwin';
+console.log(firstName);
+
+var lastName = 'Kumar';
+var age = 13;
+
+var fullAge = true;
+console.log (fullAge);
+
+var job;
+console.log(job);
+
+job ='Programmer'
+console.log(job);
+
+// Variable naming rules
+var _3years = 3;
+var ashwinKumar = 'Ashwin and Kumar'
+var if = 23;
+*/
+
+/**************************
+ * Variable mutation and type coercion
+ */
+/*
+var firstName = 'Ashwin';
+var age = 13;
+
+// Type coercion
+console.log( firstName + ' ' + age);
+
+var job, isProgrammer;
+job = 'programmer';
+isProgrammer = true;
+
+console.log(firstName + ' is a ' + age + ' years old ' + job + '.  Is he a programmer? ' + isProgrammer);
+
+
+alert('Press OK on the upcoming pop up messages to proceed.')
+alert('Welcome to Brawl Stars😀');
+alert('Due to COVID-19, we request you not to go out frequently. Stay safe during COVID-19');
+alert('Maintain Social Distance. Do not touch each other.');
+alert('Wear Masks😷 while you are going outside.');
+alert('Brawl with me on Brawl Stars during quarantine.');
+alert('Please install brawl stars by clicking on the "Get it on the Google Play" or "Download on the Apple Store"');
+alert('My Brawl Stars ID is "@brawl*is*"');
+alert('Any other queries?? Mail us at doodleacademics@gmail.com');
+alert('Earn at least 20 trophies per day');
+
+var lastName = prompt('What is your name?');
+console.log(' ' + lastName)
+
+
+/************************
+* Math Operators
+*/
+ /*
+ var year, yearAshwin, yearSunil;
+ now = 2020;
+ ageAshwin = 13;
+ ageSunil = 14;
+ yearAshwin = now - ageAshwin;
+ yearSunil = now - ageSunil;
+
+ console.log(yearAshwin);
+
+ console.log(now + 2);
+ console.log(now * 2);
+ console.log(now / 2);
+
+ //Logical Operators
+ var ashwinOlder = ageAshwin < ageSunil;
+ console.log(ashwinOlder);
+
+
+ //typeof operator
+ console.log(typeof ashwinOlder);
+ console.log(typeof ageAshwin);
+ console.log(typeof 'Sunil is older than Ashwin');
+var x
+console.log(typeof x); 
+*/
+
+/*******************
+ * Operator precedence
+ */
+/*
+ var now = 2020;
+ var yearAshwin = 2007;
+ var fullAge = 18;
+
+//Multiple Operators
+ var isFullAge = now - yearAshwin >= fullAge; //true
+ console.log(isFullAge);
+
+ // Grouping
+ var ageAshwin = now - yearAshwin;
+ var ageSunil = 14
+ var average = (ageAshwin + ageSunil)/2;
+ console.log(average);
+
+ //Multiple Assignments
+ var x, y;
+ x = y = (3 + 5) * 4 - 6; //8 * 4 - 6// 32 - 6 // 26
+ console.log(x + y);
+
+ //More Operators
+x = x * 2;
+console.log(x);
+
+x = x + 10;
+console.log(x);
+
+x++;
+console.log(x);
+
+x--;
+console.log(x);
+*/
+
+/********************************
+ * If / else statements
+ */
+
+/*
+ var firstName ='Ashwin';
+ var learningStatus = 'secondary';
+
+ if (learningStatus === 'college') {
+     console.log(firstName + ' is in college.');
+ } else {
+     console.log(firstName + ' will complete secondary education.')
+ }
+
+ var isInColllege = true;
+ if (isInColllege) {
+    console.log(firstName + ' is in college.');
+} else {
+    console.log(firstName + ' will complete secondary education.')
+}
+
+var massSunil  = 40; // kg
+var heightSunil = 153; // meters
+
+var massAshwin  = 60;
+var heightAshwin = 165; 
+
+var BMISunil = massSunil / (heightSunil  * heightSunil);
+var BMIAshwin = massAshwin / (heightAshwin * heightAshwin);
+
+
+if (BMISunil > BMIAshwin) {
+    console.log('Sunil\'s BMI is higher than Ashwin\'s. ')
+} else {
+    console.log('Ashwin\'s BMI is higher than Sunil\'s. ')
+}
+*/
+
+/*********************************
+ * Boolean Logic
+ */
+/*
+
+ var firstName = 'Reepa';
+ var age = 49;
+
+ if (age <= 5) {
+    console.log(firstName + ' is a infant.');
+ }
+ if (age <= 13 && age > 5) {
+     console.log(firstName + ' is a girl.');
+ } else if (age <= 13 && age > 20) { 
+    console.log(firstName + ' is a teenager.');
+ } else if (age >= 20 && age < 30) {
+    console.log(firstName + ' is a young girl.');
+ } else if (age >=30 && age < 50) {
+    console.log(firstName + ' is a woman.');
+ } else if (age >=50 && age < 100) {
+    console.log(firstName + ' is a grandmother.');
+ } else if (age >= 100) {
+    console.log(firstName + ' is dead.');
+ }
+
+*/
+
+/*************************
+ * The Ternary Operator and Switch Statements
+ */
+
+
+
+
+
+
+
+
+
+
+
+ 
